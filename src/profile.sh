cf9a2b7d5a5673b5100639c21ae1dca30aa95284
damshenas <you@example.com>
2021-12-27 19:00:04 +0100

    minor bug fix

--- src/profile.sh
+++ src/profile.sh
@@ -5,7 +5,7 @@ confdir=/tmp/openvpn
 dimage=damshenas/openvpn:arm64
 
 # create user cert and profile if not exist. And upload to s3. 
-if [ -f "$confdir/pki/reqs/$user.req" ]; then
+if [ ! -f "$confdir/pki/reqs/$user.req" ]; then
   docker run -v $confdir:/etc/openvpn --rm -i $dimage easyrsa build-client-full $user nopass
   docker run -v $confdir:/etc/openvpn --rm $dimage ovpn_getclient $user > $user.ovpn
   docker run -v $confdir:/etc/openvpn --rm $dimage ovpn_listclients | grep $user
